83217682d891b490f7add215025de374a0aaf19a
904284004 <904284004@L815WD150040.edu.cbe.ab.ca>
2022-03-07 09:37:10 -0700

    nd

--- chapter8/src/exercise1/UniTester.java
+++ chapter8/src/exercise1/UniTester.java
@@ -1,15 +1,63 @@
 package exercise1;
 
-public class UniTester {
+import java.text.NumberFormat;
+import java.util.Scanner;
 
-	public UniTester() {
-	
-		dadas
-	}
 
-	public static void main(String[] args) {
-	
-
-	}
+public class UniTester 
+{
+    public static void payEmployee(UEmployee emp, double payR)
+    {
+        NumberFormat money = NumberFormat.getCurrencyInstance();
+        double pay;
+        
+        System.out.println(emp);
+        pay = emp.pay(payR);
+        System.out.println(money.format(pay));
+        
+    }
+    
+    public static void main(String[] args) 
+    {
+      UEmployee emp1 = new UEmployee("Bruce", "Wayne", (double) 2000);  
+      Faculty emp2 = new Faculty("Peter", "Parker", (double) 150, "Web Developement");
+      Staff emp3 = new Staff("Darth", "Vader", (double) 600, "Full time");
+      
+      Scanner input = new Scanner(System.in);
+      
+      String action;
+      int empNum;
+      double payR;
+      
+      Employee emp = emp1;
+     do
+     { 
+      System.out.println("\nEmployee\\Pay\\Quit");
+      System.out.println("Enter choice: ");
+      action = input.next();
+     
+      if(!action.equalsIgnoreCase("Q"))
+      {
+          System.out.println("Enter Employee number (1, 2, or 3): ");
+         empNum = input.nextInt();
+         switch(empNum)
+         {
+             case 1: emp = emp1;break;
+             case 2: emp = emp2;break;
+             case 3: emp = emp3;break;
+         }
+         if(action.equalsIgnoreCase("E"))
+         {
+             System.out.println(emp);
+         }
+         else if(action.equalsIgnoreCase("P"))
+         {
+            System.out.println("Enter the hours for associate OR pay period for manager: " );
+            payR = input.nextDouble();
+            payEmployee(emp, payR);
+         }
+      }
+     }while(!action.equalsIgnoreCase("Q"));
+    }
 
 }
