a20c920ccbde297065be6a45f688562cdaa35e33
904284004 <904284004@L815WD150040.edu.cbe.ab.ca>
2022-03-15 11:55:44 -0600

    null

---
+++ chapter8/src/exercise2/AccountTester.java
@@ -0,0 +1,14 @@
+package exercise2;
+
+public class AccountTester {
+
+	public AccountTester() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
