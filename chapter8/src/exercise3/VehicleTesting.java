e36a1fb61e05764c71e405e74973fe4fb8c11b86
904284004 <904284004@L815WD220030.edu.cbe.ab.ca>
2022-03-31 11:48:31 -0600

    todo//comments n crt

--- chapter8/src/exercise3/VehicleTesting.java
+++ chapter8/src/exercise3/VehicleTesting.java
@@ -5,14 +5,36 @@ import java.util.Scanner;
 public class VehicleTesting {
 	
 	public static void main(String[] args) {
-		Car user = new Car(4, "Purple", 300, "Audi", true);
-		Minivan user2 = new Minivan(4, "Red", 205, "Honda", true );
-		Truck user3 = new Truck(6, "Black", 265, "Ford", true);
-		
+		System.out.println("Enter Vehicle to view: \n Car = 1 \n Minivan = 2 \n Truck = 3");
 		Scanner input = new Scanner(System.in);
 		int act = input.nextInt();
 		
-		System.out.println("Enter Vehicle to view: \n Car = 1 \n Minivan = 2 \n Truck = 3");
+		if (act == 1) {
+			Car user = new Car(4, "Purple", 300, "Audi", true);
+			System.out.println("Your car has " + user.getDoorsNum() + " doors" + "\nA max speed of " + user.getMaxSpeed()
+			+ " kph \nIs manufactured by " + user.getManufacture() + "\n" + user.hasNitrous());
+
+			
+		}
+		else if (act == 2) {
+			Minivan user = new Minivan(4, "Red", 205, "Honda", true );
+			System.out.println("Your minivan has " + user.getDoorsNum() + " doors" + "\nA max speed of " + user.getMaxSpeed()
+			+ " kph \nIs manufactured by " + user.getManufacture() + "\n" + user.hasSlidingDoor());
+			
+			
+		} else if (act == 3) {
+			Truck user = new Truck(6, "Black", 265, "Ford", true);
+			System.out.println("Your truck has " + user.getDoorsNum() + " doors" + "\nA max speed of " + user.getMaxSpeed()
+			+ " kph \nIs manufactured by " + user.getManufacture() + "\n" + user.isLifted());
+
+			
+		} else {
+			System.out.println("Invalid input");
+		}
+
+		
+		
+		
 	}
 
 }
