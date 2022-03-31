f450b393351d51fa7b128b6f8fffd6053406a52a
904284004 <904284004@L815WD220030.edu.cbe.ab.ca>
2022-03-31 11:25:09 -0600

    working on vehci test

--- chapter8/src/exercise3/VehicleTesting.java
+++ chapter8/src/exercise3/VehicleTesting.java
@@ -1,5 +1,18 @@
 package exercise3;
 
+import java.util.Scanner;
+
 public class VehicleTesting {
+	
+	public static void main(String[] args) {
+		Car user = new Car(4, "Purple", 300, "Audi", true);
+		Minivan user2 = new Minivan(4, "Red", 205, "Honda", true );
+		Truck user3 = new Truck(6, "Black", 265, "Ford", true);
+		
+		Scanner input = new Scanner(System.in);
+		int act = input.nextInt();
+		
+		System.out.println("Enter Vehicle to view: \n Car = 1 \n Minivan = 2 \n Truck = 3");
+	}
 
 }
