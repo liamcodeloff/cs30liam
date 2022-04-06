0ff3548ad2065506cc62f77818eb26c4302e2997
904284004 <904284004@L815WD220007.edu.cbe.ab.ca>
2022-04-06 10:16:47 -0600

    myfile2

--- chapter11/src/myFile/myFile2.java
+++ chapter11/src/myFile/myFile2.java
@@ -4,6 +4,32 @@ import java.util.Scanner;
 public class myFile2 {
 
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		File textFile = new File("c:\\myFileTester.txt");
+		if (textFile.exists()) {
+			System.out.println("File exists already");
+		} else {
+			try {
+				textFile.createNewFile();
+				System.out.println("File has been created");
+			} catch (IOException e) {
+				System.out.println("File could not be created");
+				System.err.println("IOException:" + e.getMessage());
+				
+			}
+			System.out.println("Would u like to keep or delete file?");
+			String cho = input.next();
+			if(cho.equalsIgnoreCase("Delete")) {
+				if(textFile.delete()) {
+					System.out.println("File has been deleted");
+				} else {
+					System.out.println("File has not been deleted");
+				}
+				
+			} else {
+				System.out.println("File has been kept");
+			}
+		}
 		
 		
 
