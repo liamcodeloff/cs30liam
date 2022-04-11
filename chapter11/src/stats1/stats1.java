82890635bae4b776e96d356bfb3d15585c76a2ce
904284004 <904284004@L815WD220007.edu.cbe.ab.ca>
2022-04-11 10:24:59 -0600

    s1

--- chapter11/src/stats1/stats1.java
+++ chapter11/src/stats1/stats1.java
@@ -1,7 +1,7 @@
 package stats1;
 import java.io.*;
 import java.util.Scanner;
-
+import java.util.ArrayList;
 public class stats1 {
 
 	public static void main(String[] args) {
@@ -12,28 +12,30 @@ public class stats1 {
 		String score;
 		int numScores = 0;
 		double totalScores = 0;
-		int counter = 1;
+		int count = 1;
 		
 		try {
 			s = new FileReader(scores);
 			readFile = new BufferedReader(s);
 			while ((score = readFile.readLine()) != null ) {
-				if (counter % 2 ==0) {
-					
-					numScores += 1 ;
-					totalScores += Double.parseDouble(score);
-					System.out.println(score);
 
-					
-					
-					
-	
-				}
+			if (count % 2 ==0) {
+
+				numScores += 1 ;
+				totalScores += Double.parseDouble(score);
+				System.out.println(score);
+				count +=1;
 				
-
+			}
+			else {
+				System.out.println(score+" got a");
+				count +=1;
+			}
 			}
 			avg = totalScores / numScores;
 			System.out.println("Average score is:" + avg);
+			
+
 			readFile.close();
 			s.close();
 		} catch (FileNotFoundException e) {
