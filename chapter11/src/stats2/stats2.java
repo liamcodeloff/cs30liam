aab10241e20db04783c35929c35910fd63c99398
904284004 <904284004@L815WD220007.edu.cbe.ab.ca>
2022-04-21 12:00:27 -0600

    need error

--- chapter11/src/stats2/stats2.java
+++ chapter11/src/stats2/stats2.java
@@ -11,86 +11,80 @@ public class stats2 {
 		BufferedReader readFile;
 		FileWriter out;
 		BufferedWriter bWriter;
+		PrintWriter pp;
 		double avg;
 		String score;
 		int numScores = 0;
 		double totalScores = 0;
 		int count = 1;
-		int scount;
+		int acount = 1;
 		ArrayList<Integer> list = new ArrayList<Integer>();
 		Scanner input = new Scanner(System.in);
 		
 		System.out.println("Enter name of file");
 		String file = input.next();
+		System.out.println("Enter number of students to be graded: ");
+		int scount = input.nextInt();
 		
-		try {
-		File scoresFile = new File("../Chapter11/src/stats2/" + file + ".txt");
-		FileWriter writer = new FileWriter(scoresFile);
-		
-		if (scoresFile.createNewFile()) {
-			System.out.println("File created: " + scoresFile.getName());
-		} else {
-			System.out.println("File could not be created");
-		}
-
-		} catch (IOException e) {
-			System.out.println("Error occurred");
-			e.printStackTrace();
-		}
-	
-		System.out.println("Enter number of students to be graded");
-		scount = input.nextInt();
-		
-			
-		
-			
-			
-			
+			try {
+			File scoresFile = new File("../Chapter11/src/stats2/" + file + ".txt");
+			scoresFile.createNewFile();
+			out = new FileWriter(scoresFile);
+			bWriter = new BufferedWriter(out);
+			pp = new PrintWriter(bWriter);
+			if (scoresFile.createNewFile()) {
+			} else {
 				
-		
-		
+			}
+
+			 while (scount != 0) {
+			 
+				if (acount % 2 != 0 ) {
+				System.out.println("Enter student name to be graded");
+				String name = input.nextLine();
+				input.nextLine();
+				pp.println(name);
+				pp.flush();
+				acount += 1;
 			
-		
- 
-		/* file reader  */
-		
-		/*
-		try {
-			s = new FileReader(newFile);
-			readFile = new BufferedReader(s);
-			while ((score = readFile.readLine()) != null ) {
-
-			if (count % 2 ==0) {
-
-				numScores += 1 ;
-				totalScores += Double.parseDouble(score);
-				System.out.println(score);
-				count +=1;
-				int score1 = Integer.parseInt(score);
-				list.add(score1);
-
-			}
-			else {
-				System.out.println(score+" got a");
-				count +=1;
+			}  else if (acount % 2 == 0); {
+				System.out.println("Enter student grade:");
+				int grade = input.nextInt();
+				pp.println(grade);
+				pp.flush();
+				acount += 1;
+				scount -= 1;
 			}
 			}
-			avg = totalScores / numScores;
-			System.out.println("Average Score was:" + avg);
-			Collections.sort(list);
-			int min = list.get(0);
-			int max = list.get(numScores - 1);
-			System.out.println("Highest Score was: " + max + "\nLowest Score was: " + min);
-				readFile.close();
-			s.close();
-		} catch (FileNotFoundException e) {
-			System.out.println("We ran into a problem retrieving your file " + e.getMessage());
-			System.err.println("FileNotFoundException: " + e.getMessage());
-		} catch (IOException e) {
-			System.out.println("Problem parsing file");
-			System.err.println("IOException: " + e.getMessage());
-		}
-		*/
+			 if (scount == 0) {
+				 s = new FileReader(scoresFile);
+					readFile = new BufferedReader(s);
+					while ((score = readFile.readLine()) != null ) {
 
-	}
-	}
+					if (count % 2 ==0) {
+
+						numScores += 1 ;
+						totalScores += Double.parseDouble(score);
+						System.out.println(score);
+						count +=1;
+						int score1 = Integer.parseInt(score);
+						list.add(score1);
+					}
+					else {
+						System.out.println(score+" got a");
+						count +=1;
+					}
+					}
+					avg = totalScores / numScores;
+					System.out.println("Average Score was:" + avg);
+					Collections.sort(list);
+					int min = list.get(0);
+					int max = list.get(numScores - 1);
+					System.out.println("Highest Score was: " + max + "\nLowest Score was: " + min);
+						readFile.close();
+					s.close();
+				}
+			} catch (IOException e) {
+			} 
+	}		
+}
