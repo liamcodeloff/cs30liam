13f17e9f85cbacc3c85081ec18a2cffe229d5519
904284004 <904284004@L815WD220007.edu.cbe.ab.ca>
2022-06-07 10:50:33 -0600

    p

---
+++ PhidgetsRover/src/PhidgetsChallenge.java
@@ -0,0 +1,102 @@
+import com.phidget22.*;
+
+public class PhidgetsChallenge {
+
+	public static void main(String[] args) throws Exception {
+		
+		//Connect to wireless rover
+        Net.addServer("", "192.168.100.1", 5661, "", 0);
+
+        //Create
+        DCMotor leftMotors = new DCMotor();
+        DCMotor rightMotors = new DCMotor();
+
+        //Address
+        leftMotors.setHubPort(5);
+        leftMotors.setChannel(0);
+        rightMotors.setHubPort(5);
+        rightMotors.setChannel(1);
+
+        //Open
+        leftMotors.open(5000);
+        rightMotors.open(5000);
+        
+        //Increasing acceleration 
+        leftMotors.setAcceleration(leftMotors.getMaxAcceleration());
+        rightMotors.setAcceleration(rightMotors.getMaxAcceleration());
+
+        //Move forward at full speed
+        leftMotors.setTargetVelocity(1);
+        rightMotors.setTargetVelocity(-1);
+
+        //1.62 second pause
+        Thread.sleep(1450);
+
+        leftMotors.setTargetVelocity(0);
+        rightMotors.setTargetVelocity(0);
+        
+        Thread.sleep(1200);
+        
+        //turn right
+        leftMotors.setTargetVelocity(1);
+        rightMotors.setTargetVelocity(1);
+        
+        //0.59 second pause
+        Thread.sleep(210);
+        
+        //Move forward at full speed
+        leftMotors.setTargetVelocity(1);
+        rightMotors.setTargetVelocity(-1);
+        
+        //1.62 second pause
+        Thread.sleep(1450);
+        
+        leftMotors.setTargetVelocity(0);
+        rightMotors.setTargetVelocity(0);
+        
+        Thread.sleep(1200);
+        
+        //turn right
+        leftMotors.setTargetVelocity(1);
+        rightMotors.setTargetVelocity(1);
+        
+        //0.59 second pause
+        Thread.sleep(210);
+        
+        //Move forward at full speed
+        leftMotors.setTargetVelocity(1);
+        rightMotors.setTargetVelocity(-1);
+        
+        //1.62 second pause
+        Thread.sleep(1450);
+        
+        leftMotors.setTargetVelocity(0);
+        rightMotors.setTargetVelocity(0);
+        
+        Thread.sleep(1200);
+        
+        //turn right
+        leftMotors.setTargetVelocity(1);
+        rightMotors.setTargetVelocity(1);
+        
+        //0.59 second pause
+        Thread.sleep(210);
+        
+        //Move forward at full speed
+        leftMotors.setTargetVelocity(1);
+        rightMotors.setTargetVelocity(-1);
+        
+        //1.62 second pause
+        Thread.sleep(1450);
+        
+      
+        
+        //stopping both motors
+        leftMotors.setTargetVelocity(0);
+        rightMotors.setTargetVelocity(0);
+}
+
+
+	}
+
+
---
+++ PhidgetsRover/src/test.java
@@ -0,0 +1,41 @@
+import com.phidget22.*;
+public class test {
+
+	public static void main(String[] args) throws Exception {
+		
+		//Connect to wireless rover
+        Net.addServer("", "192.168.100.1", 5661, "", 0);
+
+        //Create
+        DCMotor leftMotors = new DCMotor();
+        DCMotor rightMotors = new DCMotor();
+
+        //Address
+        leftMotors.setHubPort(5);
+        leftMotors.setChannel(0);
+        rightMotors.setHubPort(5);
+        rightMotors.setChannel(1);
+
+        //Open
+        leftMotors.open(5000);
+        rightMotors.open(5000);
+        
+        //Increasing acceleration 
+        leftMotors.setAcceleration(leftMotors.getMaxAcceleration());
+        rightMotors.setAcceleration(rightMotors.getMaxAcceleration());
+        
+      //Move forward at full speed
+        leftMotors.setTargetVelocity(1);
+        rightMotors.setTargetVelocity(-1);
+        
+        Thread.sleep(1450);
+        
+        leftMotors.setTargetVelocity(0);
+        rightMotors.setTargetVelocity(0);
+        
+        
+		
+
+	}
+
+}
