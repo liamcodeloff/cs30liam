3e8fef0809a44d126316045f5bce367df0ee6bf3
904284004 <904284004@L815WD220007.edu.cbe.ab.ca>
2022-06-08 10:11:21 -0600

    p

--- PhidgetsRover/src/PhidgetsRoverTurn.java
+++ PhidgetsRover/src/PhidgetsRoverTurn.java
@@ -7,47 +7,40 @@
 	
 		public static void main(String[] args) throws Exception
 		{
-
-			//Connect to wireless rover
 			Net.addServer("", "192.168.100.1", 5661, "", 0);
-	
-			//Create
+			
+			 //Create 
+	        DigitalInput redButton = new DigitalInput();
 	        DCMotor leftMotors = new DCMotor();
 	        DCMotor rightMotors = new DCMotor();
 	        VoltageRatioInput vAxis = new VoltageRatioInput(); 
 	        VoltageRatioInput hAxis = new VoltageRatioInput();
-	        DigitalInput button = new DigitalInput();
 	        DigitalOutput pump = new DigitalOutput();
-	        DigitalInput rbutton = new DigitalInput();
-		    Boolean buttonState = false; // track button state changes 
-
 	        
-	        //Address
+	        //Address 
+	        redButton.setHubPort(4);
+	        redButton.setIsHubPortDevice(true);
 	        leftMotors.setChannel(0);
 	        rightMotors.setChannel(1);
 	        vAxis.setChannel(1);
 	        hAxis.setChannel(0);
-	        button.setHubPort(0);
-	        pump.setHubPort(3);
 	        pump.setIsHubPortDevice(true);
-	        rbutton.setHubPort(4);
-	        rbutton.setIsHubPortDevice(true); 
-	
-	        //Open
+	        pump.setHubPort(3);
+
+	        //Open 
+	        redButton.open(1000);
 	        leftMotors.open(5000);
 	        rightMotors.open(5000);
 	        vAxis.open(5000);
 	        hAxis.open(5000);
-	        button.open(5000);
 	        pump.open(1000);
-	        rbutton.open(1000);
-	
+
 	        //Increase acceleration
 	        leftMotors.setAcceleration(leftMotors.getMaxAcceleration());
 	        rightMotors.setAcceleration(rightMotors.getMaxAcceleration());
-	
+	        
+	        //Use your Phidgets 
 	        while(true){
-	            System.out.println("Button State: " + rbutton.getState());
 	          //Get data from vertical axis (value between -1 and 1)
 	        	double verticalAxis = vAxis.getVoltageRatio();
 			          
@@ -67,7 +60,7 @@
 		        leftMotors.setTargetVelocity(leftMotorsSpeed);
 		        rightMotors.setTargetVelocity(rightMotorsSpeed);
 		        
-		        if (rbutton.getState() == true)
+		        if (redButton.getState() == true)
 		        {
 		        	pump.setState(true);
 		        }
